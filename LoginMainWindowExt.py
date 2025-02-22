78c35155e492a280c5333cd9b4354bde06969fce
ngthmduyen <nguyenduyen.231006@gmail.com>
2025-02-22 11:32:56 +0700

    libs
    libs

---
+++ LoginMainWindowExt.py
@@ -0,0 +1,10 @@
+
+class LoginMainWindowExt(Ui_MainWindow):
+    def setupUi(self, MainWindow):
+        super().setupUi(MainWindow)
+        self.MainWindow=MainWindow
+        self.setupSignalAndSlot()
+    def showWindow(self):
+        self.MainWindow.show()
+
+
